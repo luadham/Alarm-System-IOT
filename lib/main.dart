900d7bcd40368a52ce30bd4614112b81f71c711d
Mahmoud Eltelt <80259087+ElteltM@users.noreply.github.com>
2022-12-09 01:28:19 +0200

    UI first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:alarmsystem/Mqtt.dart';
 import 'package:alarmsystem/assets/UIStrings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:alarmsystem/provider/theme_provider.dart';
+import 'package:alarmsystem/widget/change_theme_button_widget.dart';
+import 'package:alarmsystem/page/home_page.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+/*class MyApp extends StatelessWidget {
   MyApp({key});
-
-  MQTTConnection connection = MQTTConnection('44.204.177.187', 2001, '\\test');
 
   @override
   Widget build(BuildContext context) {
@@ -28,4 +32,26 @@ class MyApp extends StatelessWidget {
           body: const Center(child: Text('Implement UI')),
         ));
   }
+}*/
+
+class MyApp extends StatelessWidget {
+  MQTTConnection connection = MQTTConnection('44.204.177.187', 2001, '\\test');
+
+  static const String title = 'Light & Dark Theme';
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    builder: (context, _) {
+      final themeProvider = Provider.of<ThemeProvider>(context);
+
+      return MaterialApp(
+        title: title,
+        themeMode: themeProvider.themeMode,
+        theme: MyThemes.lightTheme,
+        darkTheme: MyThemes.darkTheme,
+        home: HomePage(),
+      );
+    },
+  );
 }
