18def6ffec4e5095d9edaf9ff28bb86db7d18486
Adham Adel <luadham@gmail.com>
2022-12-13 01:35:13 +0200

    Part Of Notification is Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:alarmsystem/Mqtt.dart';
 import 'package:alarmsystem/assets/UIStrings.dart';
+import 'package:alarmsystem/service/local_notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -7,51 +8,45 @@ import 'package:alarmsystem/provider/theme_provider.dart';
 import 'package:alarmsystem/widget/change_theme_button_widget.dart';
 import 'package:alarmsystem/page/home_page.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
-/*class MyApp extends StatelessWidget {
-  MyApp({key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final LocalNotificationService _localNotificationService =
+      LocalNotificationService();
+
+  MQTTConnection connection = MQTTConnection('3.91.80.254', 2001, '\\test');
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _localNotificationService.initialize();
     connection.connect();
-
-    return MaterialApp(
-        title: UIStrings.projectName,
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Center(child: Text(UIStrings.projectName)),
-          ),
-          body: const Center(child: Text('Implement UI')),
-        ));
   }
-}*/
-
-class MyApp extends StatelessWidget {
-  MQTTConnection connection = MQTTConnection('44.204.177.187', 2001, '\\test');
-
-  static const String title = 'Light & Dark Theme';
 
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
-    create: (context) => ThemeProvider(),
-    builder: (context, _) {
-      final themeProvider = Provider.of<ThemeProvider>(context);
-
-      return MaterialApp(
-        title: title,
-        themeMode: themeProvider.themeMode,
-        theme: MyThemes.lightTheme,
-        darkTheme: MyThemes.darkTheme,
-        home: HomePage(),
+        create: (context) => ThemeProvider(),
+        builder: (context, _) {
+          final themeProvider = Provider.of<ThemeProvider>(context);
+          // connection.connect();
+          return MaterialApp(
+            title: UIStrings.title,
+            themeMode: themeProvider.themeMode,
+            debugShowCheckedModeBanner: false,
+            theme: MyThemes.lightTheme,
+            darkTheme: MyThemes.darkTheme,
+            home: HomePage(),
+          );
+        },
       );
-    },
-  );
 }
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,50 +1,55 @@
+import 'package:alarmsystem/assets/UIStrings.dart';
+import 'package:alarmsystem/service/local_notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:alarmsystem/main.dart';
 import 'package:alarmsystem/provider/theme_provider.dart';
 import 'package:alarmsystem/widget/change_theme_button_widget.dart';
 
-import 'navigationbar_widget.dart';
+// import 'navigationbar_widget.dart';
 
-class HomePage extends StatelessWidget {
-/*   @override
-   Widget build(BuildContext context) => Scaffold(
-         appBar: AppBar(
-           iconTheme: Theme.of(context).iconTheme,
-           backgroundColor: Colors.transparent,
-           leading: Icon(Icons.menu),
-           title: Text(MyApp.title),
-           elevation: 0,
-           actions: [
-             ChangeThemeButtonWidget(),
-           ],
-         ),
-         body: ProfileWidget(),
-         extendBody: true,
-         bottomNavigationBar: NavigationBarWidget(),
-       );*/
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final LocalNotificationService localNotificationService =
+      LocalNotificationService();
 
   @override
   Widget build(BuildContext context) {
     final text = Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
         ? 'DarkTheme'
         : 'LightTheme';
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orange,
-        title: Text(MyApp.title),
+        title: const Text(UIStrings.title),
         actions: [
           ChangeThemeButtonWidget(),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Text('Test'),
+        onPressed: () {
+          localNotificationService.showNotification(0, "Tets", "Warn");
+        },
+      ),
       body: Center(
-        child: Text(
-          'Hello $text!',
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hello $text!',
+              style: const TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
